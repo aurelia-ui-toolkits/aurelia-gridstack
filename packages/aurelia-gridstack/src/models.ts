abee6488626a86cc5413ddd820939bd0a2ce7e1e
Alexandr Todorov <Alexandr.Todorov@technosoft.nl>
2022-09-20 14:22:26 +0300

    gridstack item extra attributes

---
+++ packages/aurelia-gridstack/src/models.ts
@@ -0,0 +1,13 @@
+export enum ResizeHandles {
+  n = 'n',
+  ne = 'ne',
+  e = 'e',
+  se = 'se',
+  s = 's',
+  sw = 'sw',
+  w = 'w',
+  nw = 'nw'
+}
+
+export type ResizeHandleType = ResizeHandles.n | ResizeHandles.ne | ResizeHandles.e | ResizeHandles.se |
+                                ResizeHandles.s | ResizeHandles.sw | ResizeHandles.w | ResizeHandles.nw;
